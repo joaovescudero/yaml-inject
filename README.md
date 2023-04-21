706f228f00f8bfca0a98b96b635aa5b2b6009e11
Patrik <patrik@qmusicplayer.se>
2023-04-21 16:17:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# YAML Inject
+
+This is a simple npm module which injects the contents of the partial .yaml files into a specific property of the main .yaml file. See the example below.
+
+## Example
+Let's say you have the following files:
+
+`main.yaml`:
+```yaml
+title: "Team"
+players:
+description: "This is a soccer team."
+```
+`partial1.yaml`:
+```yaml
+bob:
+  name: "Bob"
+```
+`partial2.yaml`:
+```yaml
+steve:
+  name: "Steve"
+  age: 15
+```
+You can merge the partial YAML files into the players property of main.yaml by running the command:
+
+```bash
+npx yaml-inject --main main.yaml --partials 'partials/*.yaml' --output output.yaml --property players
+```
+This will generate the file `output.yaml` with the following contents:
+```yaml
+title: "Team"
+players:
+  bob:
+    name: "Bob"
+  steve:
+    name: "Steve"
+    age: 15
+description: "This is a soccer team."
+```
+
+## Usage
+You can either include [src/index.js](src/index.js) in another script and call the exported function:
+```javascript
+const yamlInject=require('yaml-inject')
+yamlInject(mainFilePath,partialsGlobString,outputFilePath,propertyPath)
+```
+or you can call the binary directly from the command line:
+
+```bash
+node yaml-merge.js --main <mainFilePath> --partials <partialsGlobString> --output <outputFilePath> --property <propertyPath>
+```
+where:
+*<mainFilePath>* is the path to the main YAML file
+*<partialsGlobString>* is the glob string to match the partial YAML files (e.g. 'partials/*.yml')
+*<outputFilePath>* is the path where the merged YAML file will be saved
+*<propertyPath>* is the property path in the main file where the partial YAML contents should be inserted (e.g. 'players')
+
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const glob = require('glob');
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+/**
+ * Inject the contents of multiple .yaml files onto a specific property of another .yaml file, outputing
+ * the result to a new .yaml file
+ * 
+ * @param {object} args
+ * @param {string} args.main       The path to main .yaml file into which injection should happen
+ * @param {string} args.property   A dot-delimited property path on the main file where the partials should be injected
+ * @param {string} args.partials   A glob string identifying the partial .yaml files
+ * @param {string} args.output     The path where the combined contents should be output
+ * 
+ * @return void
+ */
+module.exports=function yamlInject(args){
+  // Read the contents of the main YAML file and parse it into an object
+  const mainObj = yaml.safeLoad(fs.readFileSync(args.main, 'utf8'));
+
+  // Find the property on the above object which we want to modify
+  var propertyPointer = mainObj;
+  for(let p of args.property.split('.')){
+    if(p){ //ignore empty props
+      if (!propertyPointer.hasOwnProperty(p)) {
+        propertyPointer[p] = {};//If the path doesn't exist we create as we go
+      }else if(typeof propertyPointer[p]!='object'||!propertyPointer[p]){
+        exit(`The structure of ${args.main} doesn't allow for injecting at '${args.property}'. Failed while trying to access '${p}'`);
+      }
+      propertyPointer = propertyPointer[p];
+    }
+  };
+
+  // Loop through all YAML files that match the glob pattern...
+  for(let filepath of glob.sync(args.partials)){
+    //...read and parse them into objects...
+    const partialObj = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
+    
+    //...then assign that object at the propertyPointer
+    Object.assign(propertyPointer, partialObj);
+  };
+
+  // Convert the merged object to YAML and write it to the output file
+  const outputYaml = yaml.safeDump(mainObj);
+  fs.writeFileSync(args.output, outputYaml, 'utf8');
+}
