979a070c35b76e11357022d1877abea3fe3128d7
Joao Escudero <joaovescudero@gmail.com>
2024-07-12 01:51:13 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -15,51 +15,51 @@ const yaml = require('js-yaml');
  * @return string|void             If args.output is not passed then the output is returned
  */
 module.exports = function yamlInject(args) {
-	// Make sure we have all the args
-	for (const [a, aa] of Object.entries({ m: 'main', p: 'partials', o: 'output', s: 'property' })) {
-		args[aa] = args[aa] || args[a];
-		delete args[a];
-	}
-	for (const aa of ['main', 'partials']) {
-		if (!args[aa])
-			throw new Error(`Missing required argument '${aa}'`);
-	}
+  // Make sure we have all the args
+  for (const [a, aa] of Object.entries({ m: 'main', p: 'partials', o: 'output', s: 'property' })) {
+    args[aa] = args[aa] || args[a];
+    delete args[a];
+  }
+  for (const aa of ['main', 'partials']) {
+    if (!args[aa])
+      throw new Error(`Missing required argument '${aa}'`);
+  }
 
-	// Read the contents of the main YAML file and parse it into an object
-	const mainObj = yaml.load(fs.readFileSync(args.main, 'utf8'));
+  // Read the contents of the main YAML file and parse it into an object
+  const mainObj = yaml.load(fs.readFileSync(args.main, 'utf8'));
 
-	// Find the property on the above object which we want to modify
-	let propertyPointer = mainObj;
-	if (args.property) {
-		for (const p of args.property.split('.')) {
-			if (p) { // ignore empty props
-				if (Object.prototype.hasOwnProperty.call(propertyPointer, p)) {
-					propertyPointer[p] = {};// If the path doesn't exist we create as we go
-				} else if (typeof propertyPointer[p] != 'object' || !propertyPointer[p]) {
-					throw new Error(`The structure of ${args.main} doesn't allow for injecting at '${args.property}'. ` +
-						`Failed while trying to access '${p}'`);
-				}
-				propertyPointer = propertyPointer[p];
-			}
-		}
-	}
+  // Find the property on the above object which we want to modify
+  let propertyPointer = mainObj;
+  if (args.property) {
+    for (const p of args.property.split('.')) {
+      if (p) { // ignore empty props
+        if (!Object.prototype.hasOwnProperty.call(propertyPointer, p)) {
+          propertyPointer[p] = {};// If the path doesn't exist we create as we go
+        } else if (typeof propertyPointer[p] != 'object' || !propertyPointer[p]) {
+          throw new Error(`The structure of ${args.main} doesn't allow for injecting at '${args.property}'. ` +
+            `Failed while trying to access '${p}'`);
+        }
+        propertyPointer = propertyPointer[p];
+      }
+    }
+  }
 
-	// Loop through all YAML files that match the glob pattern...
-	const partialFiles = glob.sync(args.partials);
-	for (const filepath of partialFiles) {
-		// ...read and parse them into objects...
-		const partialObj = yaml.load(fs.readFileSync(filepath, 'utf8'));
+  // Loop through all YAML files that match the glob pattern...
+  const partialFiles = glob.sync(args.partials);
+  for (const filepath of partialFiles) {
+    // ...read and parse them into objects...
+    const partialObj = yaml.load(fs.readFileSync(filepath, 'utf8'));
 
-		// ...then assign that object at the propertyPointer
-		Object.assign(propertyPointer, partialObj);
-	}
+    // ...then assign that object at the propertyPointer
+    Object.assign(propertyPointer, partialObj)
+  }
 
-	// Convert the merged object to YAML
-	const outputYaml = yaml.dump(mainObj);
+  // Convert the merged object to YAML
+  const outputYaml = yaml.dump(mainObj);
 
-	// Either write to file or reutrn as string
-	if (args.output)
-		fs.writeFileSync(args.output, outputYaml, 'utf8');
-	else
-		return outputYaml;
+  // Either write to file or reutrn as string
+  if (args.output)
+    fs.writeFileSync(args.output, outputYaml, 'utf8');
+  else
+    return outputYaml;
 };
